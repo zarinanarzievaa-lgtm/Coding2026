f791629209f3e4f6401ee51e6c3c7eb38d61b0d5
Zarina <zarinanarzievaa@gmail.com>
2026-07-22 09:35:25 +0600

    Hello World

--- src/main/java/org/spring/conflict/ConflictClass.java
+++ src/main/java/org/spring/conflict/ConflictClass.java
@@ -2,6 +2,6 @@ package org.spring.conflict;
 
 public class ConflictClass {
     public static void main(String[] args) {
-        System.out.println("This is my conflict class!");
+        System.out.println("Changes in master!");
     }
 }
