850ccfdf8c6c7d27b6bf4ebb499ef6fec632d38e
Zarina <zarinanarzievaa@gmail.com>
2026-07-22 10:00:38 +0600

    Bye bye Argentina

--- src/main/java/org/spring/Argentina.java
+++ src/main/java/org/spring/Argentina.java
@@ -2,6 +2,6 @@ package org.spring;
 
 public class Argentina {
     public static void main(String[] args) {
-        System.out.println("Hello Argentina");
+        System.out.println("Bye bye Argentina");
     }
 }
