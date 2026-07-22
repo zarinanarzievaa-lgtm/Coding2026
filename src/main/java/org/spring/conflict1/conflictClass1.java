20306041f1fe46cb5c5d0988bc90828386817db6
Zarina <zarinanarzievaa@gmail.com>
2026-07-22 20:09:18 +0600

    Hello master

--- src/main/java/org/spring/conflict1/conflictClass1.java
+++ src/main/java/org/spring/conflict1/conflictClass1.java
@@ -2,6 +2,6 @@ package org.spring.conflict1;
 
 public class conflictClass1 {
     public static void main(String[] args) {
-        System.out.println("changes in master 1");
+        System.out.println("Hello master");
     }
 }
