0085ddd2a5a900931fbc7151184a8e4271d750c4
Zarina <zarinanarzievaa@gmail.com>
2026-07-22 19:56:31 +0600

    changes in master 1

--- src/main/java/org/spring/conflict1/conflictClass1.java
+++ src/main/java/org/spring/conflict1/conflictClass1.java
@@ -2,6 +2,6 @@ package org.spring.conflict1;
 
 public class conflictClass1 {
     public static void main(String[] args) {
-        System.out.println("This is my conflict class 1");
+        System.out.println("changes in master 1");
     }
 }
